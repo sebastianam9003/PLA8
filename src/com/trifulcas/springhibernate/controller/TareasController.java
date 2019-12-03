d7e23a4c0bca764ea0cca238e9a3dc67d49707ea
sebastianam9003 <56412154+sebastianam9003@users.noreply.github.com>
2019-12-03 18:43:20 +0100

    Add files via upload

--- src/com/trifulcas/springhibernate/controller/TareasController.java
+++ src/com/trifulcas/springhibernate/controller/TareasController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.trifulcas.springhibernate.entity.Contacto;
 import com.trifulcas.springhibernate.service.IContactoService;
@@ -13,6 +17,7 @@ import com.trifulcas.springhibernate.service.IContactoService;
 @Controller
 @RequestMapping("/contacto")
 public class TareasController {
+	
 	@Autowired
 	private IContactoService contactoService;
 	
@@ -24,4 +29,37 @@ public class TareasController {
 		modelo.addAttribute("contacto",contacto);
 		return"lista";
 	}
+	
+	@GetMapping("/addcontacto")
+	public String addcontacto(Model modelo) {
+		Contacto contacto = new Contacto();
+		modelo.addAttribute("contacto", contacto);
+		return "form-contacto";
+	}
+	
+	@GetMapping("updatecontacto")
+	public String updateContacto(@RequestParam("idcontacto") int idcontacto, Model modelo) {
+		
+		Contacto contacto=contactoService.getContacto(idcontacto);
+		modelo.addAttribute("contacto", contacto);
+		return "form-contacto";
+	}
+	
+	@GetMapping("deletecontacto")
+	public String deleteContacto(@RequestParam("idcontacto") int idcontacto) {
+		Contacto tarea = contactoService.getContacto(idcontacto);
+		contactoService.delete(tarea);
+
+		return "redirect:/contacto/lista";
+	}
+	
+	@PostMapping("/savecontacto")
+	public String saveContacto(@ModelAttribute("contacto") Contacto contacto) {
+
+		contactoService.save(contacto);
+		return "redirect:/contacto/lista";
+	}
+	
+	
+	
 }
