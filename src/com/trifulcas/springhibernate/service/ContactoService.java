eb954a785f184b85757040a09d418c3a7d684809
sebastianam9003 <56412154+sebastianam9003@users.noreply.github.com>
2019-12-03 18:44:15 +0100

    Add files via upload

--- src/com/trifulcas/springhibernate/service/ContactoService.java
+++ src/com/trifulcas/springhibernate/service/ContactoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+
 import com.trifulcas.springhibernate.dao.IContactoDAO;
 import com.trifulcas.springhibernate.entity.Contacto;
 
@@ -18,4 +19,25 @@ public class ContactoService implements IContactoService{
 	public List<Contacto> getContacto(){
 		return contactoDAO.getContacto();
 	}
+	
+	@Override
+	@Transactional
+	public void save(Contacto contacto) {
+		contactoDAO.save(contacto);
+		
+	}
+
+	@Override
+	@Transactional
+	public Contacto getContacto(int idcontacto) {
+		
+		return contactoDAO.getContacto(idcontacto);
+	}
+	
+	@Override
+	@Transactional
+	public 	void delete(Contacto contacto) {
+		contactoDAO.delete(contacto);
+	}
+	
 }
--- src/com/trifulcas/springhibernate/service/IContactoService.java
+++ src/com/trifulcas/springhibernate/service/IContactoService.java
@@ -4,4 +4,10 @@ import com.trifulcas.springhibernate.entity.Contacto;
 
 public interface IContactoService {
 	List<Contacto> getContacto();
+	
+	void save(Contacto contacto);
+	
+	Contacto getContacto(int idcontacto);
+	
+	void delete(Contacto idcontacto);
 }
